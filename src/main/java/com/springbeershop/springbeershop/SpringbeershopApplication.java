de4c981b82b19df0151966edd0ebbb2ce13aa22c
kossakow <tomasz.kossakowski@tomtom.com>
2019-09-11 16:59:58 +0200

    first commit

---
+++ src/main/java/com/springbeershop/springbeershop/SpringbeershopApplication.java
@@ -0,0 +1,16 @@
+package com.springbeershop.springbeershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbeershopApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringbeershopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbeershop/springbeershop/SpringbeershopApplicationTests.java
@@ -0,0 +1,19 @@
+package com.springbeershop.springbeershop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbeershopApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
