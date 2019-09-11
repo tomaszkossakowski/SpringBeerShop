9e6dd529a9a22a5862724d908a1ed2fc39dcaca4
kossakow <tomasz.kossakowski@tomtom.com>
2019-09-11 17:48:28 +0200

    remove unnecessary annotation

--- src/main/java/com/springbeershop/springbeershop/entities/User.java
+++ src/main/java/com/springbeershop/springbeershop/entities/User.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -20,7 +19,6 @@ public class User
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @PrimaryKeyJoinColumn
     @Column(name = "user_id")
     private Integer userId;
 
