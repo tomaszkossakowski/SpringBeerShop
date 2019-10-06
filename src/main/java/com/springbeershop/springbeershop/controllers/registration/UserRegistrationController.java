fb1319bc3085d763f43c62a60182f9f9d2dff11e
Przemky <przemky@gmail.com>
2019-10-06 14:21:55 +0200

    Add simple methods to registration controller

--- src/main/java/com/springbeershop/springbeershop/controllers/registration/UserRegistrationController.java
+++ src/main/java/com/springbeershop/springbeershop/controllers/registration/UserRegistrationController.java
@@ -1,14 +1,36 @@
 package com.springbeershop.springbeershop.controllers.registration;
 
+import com.springbeershop.springbeershop.entities.User;
+import com.springbeershop.springbeershop.services.registration.RegistrationUserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class UserRegistrationController {
 
-  @GetMapping(path = "/registration-page")
-  public String showRegistrationPage() {
+  private final RegistrationUserService registrationUserService;
+
+  public UserRegistrationController(RegistrationUserService registrationUserService) {
+    this.registrationUserService = registrationUserService;
+  }
+
+  @GetMapping("/registration-page")
+  public String showRegistrationPage(Model model) {
+    model.addAttribute("newUser", new User());
+
     return "registration/user-registration";
+  }
+
+  @PostMapping("/registration")
+  public String registrationUser(@ModelAttribute User user) {
+    registrationUserService.addNewUser(user);
+
+    return "index";
   }
 
 }
